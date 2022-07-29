4cf7ee7e5ab5b423f808874f87225c1cc9c79119
George Gesite <104239892+georgegesite@users.noreply.github.com>
2022-07-29 17:41:08 +0800

    working dbscan

--- DBSCAN_covid.py
+++ DBSCAN_covid.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import pygal
+import numpy as np
 from sklearn.cluster import DBSCAN
+
+dataFrame = pd.read_json(r"C:\Users\GEORGIE\Dropbox\pythong projects\JSON\MOCK2.json")
 
 def contactTracing(dataFrame, inputName):
     #Check if name is valid
-    assert (inputName in dataFrame['User'].tolist()), "User Doesn't exist"
+    assert (inputName in dataFrame['user'].tolist()), "User Doesn't exist"
     #Social distance
     safe_distance = 0.0018288 #6 feets in kilometers
     #Apply model, in case of larger dataset or noisy one, increase min_samples
@@ -16,16 +19,18 @@ def contactTracing(dataFrame, inputName):
     #Get the clusters the inputName is a part of
     inputNameClusters = set()
     for i in range(len(dataFrame)):
-        if dataFrame['User'][i] == inputName:
+        if dataFrame['user'][i] == inputName:
             inputNameClusters.add(dataFrame['Cluster'][i])
    #Get people who are in the same cluster as the inputName              
     infected = set()
     for cluster in inputNameClusters:
         if cluster != -1: #as long as it is not the -1 cluster
-            namesInCluster = dataFrame.loc[dataFrame['Cluster'] == cluster, 'User'] #Get all names in the cluster
+            namesInCluster = dataFrame.loc[dataFrame['Cluster'] == cluster, 'user'] #Get all names in the cluster
             for i in range(len(namesInCluster)):
               #locate each name on the cluster
                 name = namesInCluster.iloc[i]
                 if name != inputName: #Don't want to add the input to the results
                     infected.add(name)
     print("Potential infections are:",*infected,sep="\n" )
+
+contactTracing(dataFrame, inputName = 'Dave')
